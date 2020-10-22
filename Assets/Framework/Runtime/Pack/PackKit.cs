9e324969e9863ec2aa043205db1168e957997384
DESKTOP-3SJF1EU\Administrator <1019226202@qq.com>
2020-10-22 21:15:23 +0800

    新增单例模式

---
+++ Assets/Framework/Runtime/Pack/PackKit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Framework { 
+    public class PackKit : MonoSingleton<PackKit>
+    {
+        private static bool init = false;
+        /// <summary>
+        /// 初始化PackKit
+        /// </summary>
+        public static void Init() {
+            if (init) return;
+            init = true;
+            Instance.InitPack();
+        }
+        private void InitPack() {
+#if UNITY_EDITOR
+            if (PackSettings.SimulateAssetBundle)
+            {
+                //将ab包内的文件加入到索引中
+            }
+#endif
+            else {
+
+            }
+        }
+    }
+}
---
+++ Assets/Framework/Runtime/Utils/Singleton.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Framework {
+    interface ISingleton {
+        void OnSingletonInit();
+    }
+    public abstract class Singleton<T> : ISingleton where T :Singleton<T> ,new()
+    {
+        protected static T _Instance;
+        public static T Instance
+        {
+            get
+            {
+                if (_Instance == null) _Instance = new T();
+                return _Instance;
+            }
+        }
+        public void OnSingletonInit()
+        {
+           
+        }
+        private void Dispose()
+        {
+            _Instance = null;
+        }
+    }
+    public abstract class MonoSingleton<T> : MonoBehaviour,ISingleton where T : MonoSingleton<T>,new()
+    {
+        protected static T _Instance;
+        public static T Instance {
+            get {
+                if (_Instance == null) _Instance = new T();
+                return _Instance;
+            }
+        }
+        public void OnSingletonInit()
+        {
+          
+        }
+        private void OnDestroy()
+        {
+            _Instance = null;
+        }
+    }
+}
