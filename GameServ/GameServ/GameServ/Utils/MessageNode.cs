5849becf720027a72f20c827d1ad957f748f1d58
DESKTOP-3SJF1EU\Administrator <1019226202@qq.com>
2020-11-09 20:38:11 +0800

    测试发送queue

--- GameServ/GameServ/GameServ/Utils/MessageNode.cs
+++ GameServ/GameServ/GameServ/Utils/MessageNode.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,5 +11,6 @@ namespace GameServ.Utils
     {
        public Bufferbyte bufferbyte;
        public string methodName;
+        public Socket client;
     }
 }
--- GameServ/GameServ/GameServ/Utils/SendMessageQueue.cs
+++ GameServ/GameServ/GameServ/Utils/SendMessageQueue.cs
@@ -2,16 +2,69 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GameServ.Utils
 {
+
     /// <summary>
     /// 发送queue
     /// </summary>
     /// <typeparam name="T"></typeparam>
     class SendMessageQueue<T>
     {
+        Thread thread = null;
+        bool isRunning = false;
+        bool isWait = false;
 
+        delegate void CallBack();
+        private Queue<T> queue = null;
+     
+        CallBack callBack;
+        EventWaitHandle eventWaitHandle = new AutoResetEvent(false);
+        public SendMessageQueue() {
+
+        }
+        public void Start() {
+      
+            if (!isRunning) {
+                isRunning = true;
+                queue = new Queue<T>();
+                thread = new Thread(RunStart);
+                thread.Start();
+                callBack = new CallBack(DoRun);
+                RunStart();
+
+            }
+        }
+        public void AppednMessage(T t) {
+            queue.Enqueue(t);
+            if (!isWait) {
+                eventWaitHandle.Set();
+            }
+        }
+        private void RunStart() {
+            callBack.BeginInvoke(RunEnd, callBack);
+        }
+        private void RunEnd(IAsyncResult ac) {
+            callBack.EndInvoke(ac);
+            if (queue.Count==0) {
+                isWait = false;
+                eventWaitHandle.WaitOne();
+                isWait = true;
+            }
+            RunStart();
+        }
+        private void DoRun() {
+            
+            T t = queue.Dequeue() ;
+            if (t.GetType() == Type.GetType("GameServ.Utils.MessageNode"))
+            {
+                MessageNode node = t as MessageNode;
+                node.bufferbyte.SendMessage(node.client);
+
+            }
+        }
     }
 }
